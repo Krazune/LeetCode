85109424f60b2e949f0a82cb828a6176d5ff468d
Krazune <krazune@gmail.com>
2018-02-09 10:50:17 +0000

    Add the solution for problem 434.

---
+++ Projects/problem_434/c++/solution.cpp
@@ -0,0 +1,31 @@
+// 434. Number of Segments in a String
+#include <cstdlib>
+#include <string>
+
+class Solution
+{
+    public:
+    int countSegments(std::string input)
+    {
+        bool inSegment = false;
+        int segmentCount = 0;
+        
+        for (char character : input)
+        {
+            if (character != ' ')
+            {
+                if (!inSegment)
+                {
+                    inSegment = true;
+                    segmentCount++;
+                }
+            }
+            else
+            {
+                inSegment = false;
+            }
+        }
+        
+        return segmentCount;
+    }
+};
