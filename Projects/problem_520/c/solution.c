55d5ce591a5423d63914ef5f1de2869eaefb608f
Krazune <krazune@gmail.com>
2018-02-23 19:29:49 +0000

    Correct formatting

--- Projects/problem_520/c/solution.c
+++ Projects/problem_520/c/solution.c
@@ -8,12 +8,10 @@ int detectCapitalUse(char* word)
         if (isupper(word[1]))
         {
             int i = 2;
+
             for ( ; word[i] != '\0'; i++)
             {
-                if (islower(word[i]))
-                {
-                    return 0;
-                }
+                if (islower(word[i])) return 0;
             }
             
             return 1;
@@ -21,12 +19,10 @@ int detectCapitalUse(char* word)
         else
         {
             int i = 1;
+
             for ( ; word[i] != '\0'; i++)
             {
-                if (isupper(word[i]))
-                {
-                    return 0;
-                }
+                if (isupper(word[i])) return 0;
             }
             
             return 1;
@@ -35,12 +31,10 @@ int detectCapitalUse(char* word)
     else
     {
         int i = 0;
+
         for ( ; word[i] != '\0'; i++)
         {
-            if (isupper(word[i]))
-            {
-                return 0;
-            }
+            if (isupper(word[i])) return 0;
         }
         
         return 1;
