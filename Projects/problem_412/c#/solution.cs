7186f6e479641eae498abf3d81a3909f99e2ea33
Krazune <krazune@gmail.com>
2018-02-24 22:43:49 +0000

    Refactor solution

--- Projects/problem_412/c#/solution.cs
+++ Projects/problem_412/c#/solution.cs
@@ -1,29 +1,36 @@
 // 412. Fizz Buzz
 using System;
+using System.Collections;
 
 public class Solution
 {
-	public IList<String> FizzBuzz(int quantity)
-	{
-		IList<String> wordList = new List<String>();
+    public IList<String> FizzBuzz(int quantity)
+    {
+        IList<String> wordList = new List<String>();
 
-		for (int number = 0; number < quantity; number++)
-		{
-			if ((number + 1) % 15 == 0)
-			{
-				wordList.Add("FizzBuzz");
-			}
-			else if ((number + 1) % 3 == 0) {
-				wordList.Add("Fizz");
-			}
-			else if ((number + 1) % 5 == 0)
-			{
-				wordList.Add("Buzz");
-			} else
-			{
-				wordList.Add((number + 1).ToString());
-			}
-		}
-		return wordList;
-	}
+        for (int number = 0, fizz = 1, buzz = 1; number < quantity; number++, fizz++, buzz++)
+        {
+            if (fizz == 3)
+            {
+                wordList.Add("Fizz");
+                fizz = 0;
+                
+                if (buzz == 5)
+                {
+                    wordList[number] += "Buzz";
+                    buzz = 0;
+                }
+            }
+            else if (buzz == 5)
+            {
+                wordList.Add("Buzz");
+                buzz = 0;
+            }
+            else
+            {
+                wordList.Add((number + 1).ToString());
+            }
+        }
+        return wordList;
+    }
 }
