a5f0749d5569dec73cd1759812481278424a84fb
Krazune <krazune@gmail.com>
2018-02-24 19:17:58 +0000

    Correct formatting

--- Projects/problem_27/c/solution.c
+++ Projects/problem_27/c/solution.c
@@ -1,19 +1,12 @@
 // 27. Remove Element
 int removeElement(int* numbers, int size, int value)
 {
-    int o = 0;
-    int i = 0;
+    int o = 0, i = 0;
     
     for ( ; i < size; i++)
     {
-        if (numbers[i] == value)
-        {
-            o++;
-        }
-        else if (o > 0)
-        {
-            numbers[i - o] = numbers[i];
-        }
+        if (numbers[i] == value) o++;
+        else if (o > 0) numbers[i - o] = numbers[i];
     }
     
     return size - o;
