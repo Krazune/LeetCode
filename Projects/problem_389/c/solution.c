16fbac7b8a7dd73c67c68c608e675648dc6e3a4b
Krazune <krazune@gmail.com>
2018-02-26 19:16:16 +0000

    Refactor solution

--- Projects/problem_389/c/solution.c
+++ Projects/problem_389/c/solution.c
@@ -1,14 +1,11 @@
 // 389. Find the Difference
 char findTheDifference(char* s, char* t)
 {
-        int n = 0;
-        int i = 0;
-        
-        for (; s[i] != 0; i++)
-        {
-            n -= s[i];
-            n += t[i];
-        }
+    int i = 0, n = 0;
+
+    for (; s[i] != 0; i++) n ^= s[i] ^ t[i];
     
-        return (n + t[i]);
+    n ^= t[i];
+
+    return n;
 }
