09b33309a515a40fec0aebe576eb7db068a7139a
Krazune <krazune@gmail.com>
2018-03-01 15:54:29 +0000

    Add problem 226 in C++

---
+++ Projects/problem_226/c++/solution.cpp
@@ -0,0 +1,19 @@
+// 226. Invert Binary Tree
+class Solution
+{
+    public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if (root == NULL) return NULL;
+
+        TreeNode* t = root->left;
+
+        root->left = root->right;
+        root->right = t;
+
+        if (root->left != NULL) invertTree(root->left);
+        if (root->right != NULL) invertTree(root->right);
+
+        return root;
+    }
+};
