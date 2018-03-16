beb66ed1e36f772ce357bc40d9f5ac5d8305bdab
Krazune <krazune@gmail.com>
2018-03-16 21:41:27 +0000

    Add solution 100 in C

---
+++ Projects/problem_100/c/solution.c
@@ -0,0 +1,17 @@
+// 100. Same Tree
+#include <cstddef>
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+
+    if ((p == NULL || q == NULL) || (p->val != q->val))
+    {
+        return false;
+    }
+    
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
