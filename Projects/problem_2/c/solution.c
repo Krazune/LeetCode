c64b1949a43ea32e4767b00e4c30e71f1d796479
Krazune <krazune@gmail.com>
2018-03-25 13:27:49 +0100

    Refactor solution 2 in C

--- Projects/problem_2/c/solution.c
+++ Projects/problem_2/c/solution.c
@@ -5,7 +5,7 @@ struct ListNode* addTwoNumbers(struct ListNode* list1, struct ListNode* list2)
 {
 	if (list1 == NULL && list2 == NULL) return NULL;
 	
-	struct ListNode* r = malloc(sizeof(struct ListNode)), *n = r, *b = NULL, *l1 = list1, *l2 = list2;
+	struct ListNode* r = malloc(sizeof(struct ListNode)), *n = r, *p = NULL, *l1 = list1, *l2 = list2;
 	int c = 0;
 	
 	while (l1 != NULL || l2 != NULL)
@@ -24,22 +24,22 @@ struct ListNode* addTwoNumbers(struct ListNode* list1, struct ListNode* list2)
 			l2 = l2->next;
 		}
 		
-		b = n;
-		s = (d1 + d2) % 10;
-		n->val = (s + c) % 10;
+		p = n;
+		s = d1 + d2 + c;
+		n->val = s % 10;
 		n->next = malloc(sizeof(struct ListNode));
 		n = n->next;
-		c = (d1 + d2 + c > 9) ? 1 : 0;
+		c = s / 10;
 	}
 	
-	if (c > 0)
+	if (c == 1)
 	{
 		n->val = 1;
 		n->next = NULL;
 	}
 	else
 	{
-		b->next = NULL;
+		p->next = NULL;
 		free(n);
 	}
 	
