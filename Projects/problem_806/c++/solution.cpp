f007b67fefc758e68b9d4de6d6e5da0076bc3e82
Krazune <krazune@gmail.com>
2018-03-30 13:01:05 +0100

    Add solution 806 in C++

---
+++ Projects/problem_806/c++/solution.cpp
@@ -0,0 +1,30 @@
+// 806. Number of Lines To Write String
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+    public:
+    vector<int> numberOfLines(vector<int>& widths, string S)
+    {
+        vector<int> r(2, 1);
+        int l = 0;
+        
+        for (char c : S)
+        {
+            int w = widths[c - 'a'];
+            
+            if (l + w > 100)
+            {
+                r[0]++;
+                l = w;
+            }
+            else l += w;
+        }
+        
+        r[1] = l;
+        
+        return r;
+    }
+};
