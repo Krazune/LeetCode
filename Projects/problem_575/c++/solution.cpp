cb0789ac4acc80ea545602243f5c86d3b73f7cbf
Krazune <krazune@gmail.com>
2018-04-01 13:30:17 +0100

    Correct indentation

--- Projects/problem_575/c++/solution.cpp
+++ Projects/problem_575/c++/solution.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 class Solution
 {
-    public:
-    int distributeCandies(vector<int>& candies)
-    {
-        unordered_set<int> c(candies.begin(), candies.end());
-        int h = candies.size() / 2;
-        
-        return h > c.size() ? c.size() : h;
-    }
+	public:
+	int distributeCandies(vector<int>& candies)
+	{
+		unordered_set<int> c(candies.begin(), candies.end());
+		int h = candies.size() / 2;
+		
+		return h > c.size() ? c.size() : h;
+	}
 };
--- Projects/problem_806/c/solution.c
+++ Projects/problem_806/c/solution.c
@@ -3,25 +3,25 @@
 
 int* numberOfLines(int* widths, int widthsSize, char* S, int* returnSize)
 {
-    int* r = malloc(sizeof(int) * 2);
-    int l = 0;
-    
-    r[0] = r[1] = 1;
-    *returnSize = 2;
+	int* r = malloc(sizeof(int) * 2);
+	int l = 0;
+	
+	r[0] = r[1] = 1;
+	*returnSize = 2;
 
-    for (int i = 0; S[i] != '\0'; i++)
-    {
-        int w = widths[S[i] - 'a'];
+	for (int i = 0; S[i] != '\0'; i++)
+	{
+		int w = widths[S[i] - 'a'];
 
-        if (l + w > 100)
-        {
-            r[0]++;
-            l = w;
-        }
-        else l += w;
-    }
+		if (l + w > 100)
+		{
+			r[0]++;
+			l = w;
+		}
+		else l += w;
+	}
 
-    r[1] = l;
+	r[1] = l;
 
-    return r;
+	return r;
 }
