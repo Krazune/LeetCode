4250325fec6a3c6d0ddabc3bc3ba6734154faf49
Krazune <krazune@gmail.com>
2018-04-23 13:52:05 +0100

    Correct formatting

--- problems/387/c/solution.c
+++ problems/387/c/solution.c
@@ -3,17 +3,11 @@ int firstUniqChar(char* s)
 {
 	int a[26] = { 0 };
 
-	for (int i = 0; s[i] != '\0'; i++)
-	{
-		a[s[i] - 'a']++;
-	}
+	for (int i = 0; s[i] != '\0'; i++) a[s[i] - 'a']++;
 
 	for (int i = 0; s[i] != '\0'; i++)
 	{
-		if (a[s[i] - 'a'] == 1)
-		{
-			return i;
-		}
+		if (a[s[i] - 'a'] == 1) return i;
 	}
 
 	return -1;
