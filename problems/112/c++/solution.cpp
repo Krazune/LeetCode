c8101725d900830482cca77888bf32ec4fdcf0f9
Krazune <krazune@gmail.com>
2018-04-24 12:49:28 +0100

    Correct formatting

--- problems/112/c++/solution.cpp
+++ problems/112/c++/solution.cpp
@@ -6,17 +6,11 @@ class Solution
 	public:
 	bool hasPathSum(TreeNode* root, int sum)
 	{
-		if (root == NULL)
-		{
-			return false;
-		}
+		if (root == NULL) return false;
 		
 		int s = sum - root->val;
 
-		if (root->left == NULL && root->right == NULL)
-		{
-			return s == 0;
-		}
+		if (root->left == NULL && root->right == NULL) return s == 0;
 
 		return hasPathSum(root->left, s) || hasPathSum(root->right, s);
 	}
