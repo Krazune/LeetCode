f2b4459bf9d77f87767dc370404be535dc1920dc
Krazune <krazune@gmail.com>
2018-04-29 13:27:35 +0100

    Update formatting

--- problems/268/c/solution.c
+++ problems/268/c/solution.c
@@ -1,9 +1,12 @@
 // 268. Missing Number
 int missingNumber(int* nums, int numsSize)
 {
-	int s = numsSize * (numsSize + 1) / 2;
-	
-	for (int i = 0; i < numsSize; i++) s -= nums[i];
-	
-	return s;
+	int missing = numsSize * (numsSize + 1) / 2;
+
+	for (int index = 0; index < numsSize; index++)
+	{
+		missing -= nums[index];
+	}
+
+	return missing;
 }
