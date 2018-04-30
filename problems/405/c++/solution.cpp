d6c06a0406be5cca7de1ddc40f49d91856cbf13a
Krazune <krazune@gmail.com>
2018-04-30 13:35:35 +0100

    Update formatting

--- problems/405/c++/solution.cpp
+++ problems/405/c++/solution.cpp
@@ -8,18 +8,21 @@ class Solution
 	public:
 	string toHex(int num)
 	{
-		if (num == 0) return "0";
-		
-		string h;
-		const char d[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-		unsigned int n = num;
-			
-		while (n != 0)
+		if (num == 0)
 		{
-			h = d[n % 16] + h;
-			n /= 16;
+			return "0";
 		}
-		
-		return h;
+
+		string hex;
+		const char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+		unsigned int number = num;
+
+		while (number != 0)
+		{
+			hex = digits[number % 16] + hex;
+			number /= 16;
+		}
+
+		return hex;
 	}
 };
