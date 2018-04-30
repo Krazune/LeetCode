a5ca29145dbf71badab2abe00f5ba44f3fe5e2b6
Krazune <krazune@gmail.com>
2018-04-30 13:27:15 +0100

    Update formatting

--- problems/404/c/solution.c
+++ problems/404/c/solution.c
@@ -3,8 +3,17 @@
 
 int sumOfLeftLeaves(struct TreeNode* root)
 {
-	if (root == NULL) return 0;
+	if (root == NULL)
+	{
+		return 0;
+	}
 
-	if (root->left != NULL && root->left->left == NULL && root->left->right == NULL) return root->left->val + sumOfLeftLeaves(root->right);
-	else return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+	if (root->left != NULL && root->left->left == NULL && root->left->right == NULL)
+	{
+		return root->left->val + sumOfLeftLeaves(root->right);
+	}
+	else
+	{
+		return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+	}
 }
