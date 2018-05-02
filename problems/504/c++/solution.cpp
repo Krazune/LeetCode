efd5d0df05c34873e570afa3602e7fbefede96e4
Krazune <krazune@gmail.com>
2018-05-02 12:52:42 +0100

    Update formatting

--- problems/504/c++/solution.cpp
+++ problems/504/c++/solution.cpp
@@ -8,19 +8,25 @@ class Solution
 	public:
 	string convertToBase7(int num)
 	{
-		if (num == 0) return "0";
-		
-		int a = abs(num);
-		string r;
-		
-		while (a != 0)
+		if (num == 0)
 		{
-			r = to_string(a % 7) + r;
-			a /= 7;
+			return "0";
 		}
-		
-		if (num < 0) r = '-' + r;
-		
-		return r;
+
+		int absoluteNumber = abs(num);
+		string base7;
+
+		while (absoluteNumber != 0)
+		{
+			base7 = to_string(absoluteNumber % 7) + base7;
+			absoluteNumber /= 7;
+		}
+
+		if (num < 0)
+		{
+			base7 = '-' + base7;
+		}
+
+		return base7;
 	}
 };
