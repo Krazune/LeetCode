be514cac25a0c46a51eb13994c7b33bc6587bc68
Krazune <krazune@gmail.com>
2018-05-02 13:38:04 +0100

    Update formatting

--- problems/606/c++/solution.cpp
+++ problems/606/c++/solution.cpp
@@ -9,17 +9,23 @@ class Solution
 	public:
 	string tree2str(TreeNode* t)
 	{
-		if (t == NULL) return "";
-		
-		string s = to_string(t->val);
-		
+		if (t == NULL)
+		{
+			return "";
+		}
+
+		string stringValue = to_string(t->val);
+
 		if (t->right != NULL)
 		{
-			s += "(" + tree2str(t->left) + ")";
-			s += "(" + tree2str(t->right) + ")";
+			stringValue += "(" + tree2str(t->left) + ")";
+			stringValue += "(" + tree2str(t->right) + ")";
 		}
-		else if (t->left != NULL) s += "(" + tree2str(t->left) + ")";
-		
-		return s;
+		else if (t->left != NULL)
+		{
+			stringValue += "(" + tree2str(t->left) + ")";
+		}
+
+		return stringValue;
 	}
 };
