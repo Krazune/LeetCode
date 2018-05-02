9be05a649544835612517333dc6a14997beab5ca
Krazune <krazune@gmail.com>
2018-05-02 13:25:37 +0100

    Update formatting

--- problems/566/c++/solution.cpp
+++ problems/566/c++/solution.cpp
@@ -8,21 +8,24 @@ class Solution
 	public:
 	vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c)
 	{
-		if (nums.size() * nums[0].size() != r * c) return nums;
-
-		vector<vector<int>> m(r, vector<int>(c));
-		int w = nums[0].size();
-
-		for (int i = 0; i < r; i++)
+		if (nums.size() * nums[0].size() != r * c)
 		{
-			for (int j = 0; j < c; j++)
-			{
-				int l = i * c + j;
+			return nums;
+		}
 
-				m[i][j] = nums[l / w][l % w];
+		vector<vector<int>> matrix(r, vector<int>(c));
+		int columnCount = nums[0].size();
+
+		for (int row = 0; row < r; row++)
+		{
+			for (int column = 0; column < c; column++)
+			{
+				int unidimensionalIndex = row * c + column;
+
+				matrix[row][column] = nums[unidimensionalIndex / columnCount][unidimensionalIndex % columnCount];
 			}
 		}
 
-		return m;
+		return matrix;
 	}
 };
