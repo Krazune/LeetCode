59ca94185de8949631ae9c86b119ca8b2910d4d7
Krazune <krazune@gmail.com>
2018-05-02 13:32:23 +0100

    Update formatting

--- problems/575/c++/solution.cpp
+++ problems/575/c++/solution.cpp
@@ -9,9 +9,9 @@ class Solution
 	public:
 	int distributeCandies(vector<int>& candies)
 	{
-		unordered_set<int> c(candies.begin(), candies.end());
-		int h = candies.size() / 2;
-		
-		return h > c.size() ? c.size() : h;
+		unordered_set<int> candyDistributions(candies.begin(), candies.end());
+		int halfCount = candies.size() / 2;
+
+		return halfCount > candyDistributions.size() ? candyDistributions.size() : halfCount;
 	}
 };
