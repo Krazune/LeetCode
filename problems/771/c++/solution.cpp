286c6114f4b7d62b20ce921a8c6696cc15ff8a81
Krazune <krazune@gmail.com>
2018-05-05 12:55:25 +0100

    Update formatting

--- problems/771/c++/solution.cpp
+++ problems/771/c++/solution.cpp
@@ -9,14 +9,17 @@ class Solution
 	public:
 	int numJewelsInStones(string jewels, string stones)
 	{
-		unordered_set<char> j(jewels.begin(), jewels.end());
-		int c = 0;
-		
-		for (int i = 0; i < stones.size(); i++)
+		unordered_set<char> existingJewels(jewels.begin(), jewels.end());
+		int count = 0;
+
+		for (int index = 0; index < stones.size(); index++)
 		{
-			if (j.find(stones[i]) != j.end()) c++;
+			if (existingJewels.find(stones[index]) != existingJewels.end())
+			{
+				count++;
+			}
 		}
-		
-		return c;
+
+		return count;
 	}
 };
