00f4c809d95b22e47889790cb32247bea05c38f4
Krazune <krazune@gmail.com>
2018-05-06 13:11:54 +0100

    Update formatting

--- problems/814/c++/solution.cpp
+++ problems/814/c++/solution.cpp
@@ -6,12 +6,21 @@ class Solution
 	public:
 	TreeNode* pruneTree(TreeNode* root)
 	{
-		if (root == NULL) return NULL;
-		
+		if (root == NULL)
+		{
+			return NULL;
+		}
+
 		root->left = pruneTree(root->left);
 		root->right = pruneTree(root->right);
-		
-		if (root->left == NULL && root->right == NULL && root->val == 0) return NULL;
-		else return root;
+
+		if (root->left == NULL && root->right == NULL && root->val == 0)
+		{
+			return NULL;
+		}
+		else
+		{
+			return root;
+		}
 	}
 };
