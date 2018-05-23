ecb2462b522f192c17cfe278ec4f667c5356a256
Krazune <krazune@gmail.com>
2018-05-23 12:53:34 +0100

    Correct identantion

--- problems/106/c++/solution.cpp
+++ problems/106/c++/solution.cpp
@@ -7,53 +7,53 @@ using namespace std;
 
 class Solution
 {
-    public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
-    {
-        int nodeCount = inorder.size();
+	public:
+	TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
+	{
+		int nodeCount = inorder.size();
 
-        if (nodeCount == 0)
-        {
-            return NULL;
-        }
+		if (nodeCount == 0)
+		{
+			return NULL;
+		}
 
-        unordered_map<int, int> nodeIndices;
-        TreeNode* root = new TreeNode(postorder[nodeCount - 1]);
-        stack<TreeNode*> parents;
+		unordered_map<int, int> nodeIndices;
+		TreeNode* root = new TreeNode(postorder[nodeCount - 1]);
+		stack<TreeNode*> parents;
 
-        for (int nodeIndex = 0; nodeIndex < nodeCount; nodeIndex++)
-        {
-            nodeIndices[inorder[nodeIndex]] = nodeIndex;
-        }
-        
-        parents.push(root);
-        
-        for (int postorderIndex = nodeCount - 2; postorderIndex >= 0; postorderIndex--)
-        {
-            TreeNode* newNode = new TreeNode(postorder[postorderIndex]);
-            
-            if (nodeIndices[postorder[postorderIndex]] > nodeIndices[parents.top()->val])
-            {
-                parents.top()->right = newNode;
-            }
-            else
-            {
-                TreeNode* parent = parents.top();
-                
-                parents.pop();
+		for (int nodeIndex = 0; nodeIndex < nodeCount; nodeIndex++)
+		{
+			nodeIndices[inorder[nodeIndex]] = nodeIndex;
+		}
+		
+		parents.push(root);
+		
+		for (int postorderIndex = nodeCount - 2; postorderIndex >= 0; postorderIndex--)
+		{
+			TreeNode* newNode = new TreeNode(postorder[postorderIndex]);
+			
+			if (nodeIndices[postorder[postorderIndex]] > nodeIndices[parents.top()->val])
+			{
+				parents.top()->right = newNode;
+			}
+			else
+			{
+				TreeNode* parent = parents.top();
+				
+				parents.pop();
 
-                while (!parents.empty() && nodeIndices[postorder[postorderIndex]] < nodeIndices[parents.top()->val])
-                {
-                    parent = parents.top();
-                    parents.pop();
-                }
-                
-                parent->left = newNode;
-            }
-            
-            parents.push(newNode);
-        }
+				while (!parents.empty() && nodeIndices[postorder[postorderIndex]] < nodeIndices[parents.top()->val])
+				{
+					parent = parents.top();
+					parents.pop();
+				}
+				
+				parent->left = newNode;
+			}
+			
+			parents.push(newNode);
+		}
 
-        return root;
-    }
+		return root;
+	}
 };
