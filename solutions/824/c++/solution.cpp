12b9a9dd4c8eb389bae3913a784601945181adb5
Krazune <krazune@gmail.com>
2018-12-20 10:59:59 +0000

    Improve solution

--- solutions/824/c++/solution.cpp
+++ solutions/824/c++/solution.cpp
@@ -20,47 +20,45 @@ class Solution
 		int stringSize = S.size();
 		string currentWord;
 		int wordCount = 0;
+		int lastIndex = stringSize - 1;
 
 		for (int characterIndex = 0; characterIndex < stringSize; ++characterIndex)
 		{
 			char currentCharacter = S[characterIndex];
 
-			if (currentCharacter == ' ' || characterIndex == stringSize - 1)
-			{
-				// WIP
-				if (characterIndex == stringSize - 1)
-				{
-					currentWord += currentCharacter;
-				}
-
-				++wordCount;
-
-				char firstLetter = currentWord[0];
-
-				if (!isVowel(firstLetter))
-				{
-					currentWord = currentWord.substr(1, currentWord.size() - 1) + firstLetter;
-				}
-
-				currentWord += "ma";
-
-				for (int a = 0; a < wordCount; ++a)
-				{
-					currentWord += 'a';
-				}
-
-				if (wordCount > 1)
-				{
-					translation += ' ';
-				}
-
-				translation += currentWord;
-				currentWord.clear();
-			}
-			else
+			if (currentCharacter != ' ')
 			{
 				currentWord += currentCharacter;
+
+				if (characterIndex < lastIndex)
+				{
+					continue;
+				}
 			}
+
+			++wordCount;
+
+			char firstLetter = currentWord[0];
+
+			if (!isVowel(firstLetter))
+			{
+				currentWord = currentWord.substr(1, currentWord.size() - 1) + firstLetter;
+			}
+
+			currentWord += "ma";
+
+			for (int a = 0; a < wordCount; ++a)
+			{
+				currentWord += 'a';
+			}
+
+			if (wordCount > 1)
+			{
+				translation += ' ';
+			}
+
+			translation += currentWord;
+			currentWord.clear();
 		}
 
 		return translation;
