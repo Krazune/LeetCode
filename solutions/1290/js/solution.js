c231f68ccdba72560093ed4057c91695cdebfd45
Krazune <krazune@gmail.com>
2020-10-17 22:22:48 +0100

    Correct solution

--- solutions/1290/js/solution.js
+++ solutions/1290/js/solution.js
@@ -1,25 +1,14 @@
 // 1290. Convert Binary Number in a Linked List to Integer
 var getDecimalValue = function(head)
 {
-	let digitStack = new Array();
+	let result = head.val;
+	let node = head.next;
 
-	for (let node = head; node != null; node = node.next)
+	while (node != null)
 	{
-		digitStack.push(node.val);
-	}
-
-	let digitCount = digitStack.length;
-	let result = 0;
-	let multiplier = 1;
-
-	for (let i = 0; i < digitCount; ++i)
-	{
-		if (digitStack.pop() == 1)
-		{
-			result += multiplier;
-		}
-
-		multiplier *= 2;
+		result *= 2;
+		result += node.val;
+		node = node.next;
 	}
 
 	return result;
