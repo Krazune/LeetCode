7b48d350b4aa3aa962b2f90a350bf2840da4dde4
Krazune <krazune@gmail.com>
2020-10-22 11:23:26 +0100

    Add solution 478 in JavaScript

---
+++ solutions/478/js/solution.js
@@ -0,0 +1,20 @@
+// 478. Generate Random Point in a Circle
+var Solution = function(radius, x_center, y_center)
+{
+	this._radius = radius;
+	this._x = x_center;
+	this._y = y_center;
+};
+
+Solution.prototype.randPoint = function()
+{
+	let randomRadian = Math.random() * Math.PI * 2;
+	let distance = Math.sqrt(Math.random()) * this._radius; // Doesn't include the cinrcumference.
+
+	let point = new Array(2);
+
+	point[0] = this._x + distance * Math.cos(randomRadian);
+	point[1] = this._y + distance * Math.sin(randomRadian);
+
+	return point;
+};
