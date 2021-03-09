89b7cb6f6b711e77af2e346b9cc1e01434778aa8
Krazune <krazune@gmail.com>
2021-03-09 13:45:56 +0000

    Add solution 1736 in C++

---
+++ solutions/1736/c++/solution.cpp
@@ -0,0 +1,56 @@
+// 1736. Latest Time by Replacing Hidden Digits
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+	public:
+	string maximumTime(string time)
+	{
+		string result(time);
+
+		if (result[0] == '?')
+		{
+			if (result[1] == '?')
+			{
+				result[0] = '2';
+				result[1] = '3';
+			}
+			else if (result[1] == '0' || result[1] == '1' || result[1] == '2' || result[1] == '3')
+			{
+				result[0] = '2';
+			}
+			else
+			{
+				result[0] = '1';
+			}
+		}
+		else if (result[0] == '0' || result[0] == '1')
+		{
+			if (result[1] == '?')
+			{
+				result[1] = '9';
+			}
+		}
+		else
+		{
+			if (result[1] == '?')
+			{
+				result[1] = '3';
+			}
+		}
+
+		if (result[3] == '?')
+		{
+			result[3] = '5';
+		}
+
+		if (result[4] == '?')
+		{
+			result[4] = '9';
+		}
+
+		return result;
+	}
+};
