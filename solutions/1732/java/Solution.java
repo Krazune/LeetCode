9cec6f89ae7fd6d4a759f5c1d94866b25ca51464
Krazune <krazune@gmail.com>
2021-03-10 13:19:53 +0000

    Add solution 1732 in Java

---
+++ solutions/1732/java/Solution.java
@@ -0,0 +1,21 @@
+// 1732. Find the Highest Altitude
+class Solution
+{
+	public int largestAltitude(int[] gain)
+	{
+		int highest = 0;
+		int currentAltitude = 0;
+
+		for (int i = 0; i < gain.length; ++i)
+		{
+			currentAltitude += gain[i];
+
+			if (currentAltitude > highest)
+			{
+				highest = currentAltitude;
+			}
+		}
+
+		return highest;
+	}
+}
