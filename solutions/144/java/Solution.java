ab8549a27a6f14b7a4237fdfb0e056d89d4db58d
Krazune <krazune@gmail.com>
2023-05-31 18:47:46 +0100

    Correct class header

--- solutions/144/java/Solution.java
+++ solutions/144/java/Solution.java
@@ -3,7 +3,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Application
+class Solution
 {
 	public List<Integer> preorderTraversal(TreeNode root)
 	{
